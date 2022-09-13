6e99e4d041b43163b2a8ed4a5c8d66c297f46347
LilRaffl <caninosbrancoscb10@gmail.com>
2022-09-13 15:58:42 -0300

    finalizado

--- app/src/main/java/br/com/senac/mycaf/MainActivity.java
+++ app/src/main/java/br/com/senac/mycaf/MainActivity.java
@@ -1,10 +1,14 @@
 package br.com.senac.mycaf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarItemView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +18,50 @@ public class MainActivity extends AppCompatActivity {
     cafeFragment cafeFragment = new cafeFragment();
     Fragment_cha fragment_cha = new Fragment_cha();
     Fragment_donuts fragment_donuts = new Fragment_donuts();
+    Fragment_muffin fragment_muffin = new Fragment_muffin();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        // carregando do fragmente (home)
+        getSupportFragmentManager().beginTransaction().replace(R.id.frmContainer, cafeFragment).commit();
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.mCof:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frmContainer, cafeFragment)
+                                .commit();
+                        return true;
+                    case R.id.mMuffin:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frmContainer, fragment_muffin)
+                                .commit();
+                        return true;
+                    case R.id.mCha:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frmContainer,fragment_cha )
+                                .commit();
+
+                        return true;
+                    case R.id.donuts:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frmContainer,fragment_donuts )
+                                .commit();
+                        return true;
+
+
+                }
+
+                return false;
+            }
+        });
+
+
     }
 }
